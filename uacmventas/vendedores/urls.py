d5967951c55ca2e9c9e2caeb12e161a672fe0b51
Jonathan Bernal <bernaljonathan179@gmail.com>
2024-03-03 23:07:08 -0600

    Creacion formulario

--- uacmventas/vendedores/urls.py
+++ uacmventas/vendedores/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.lista_vendedores, name='lista_vendedores'),
     path('<int:vendedor_id>/', views.detalle_vendedor, name='detalle_vendedor'),
+    path('crear-vendedor/', views.crear_vendedor, name='crear_vendedor'),
+    path('crear-articulo/', views.crear_articulo, name='crear_articulo'),
 ]
